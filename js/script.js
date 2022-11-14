0d4fc62166ec62c7adec3bd0cb42bb3f309698de
kacperlNYC <113933880+kacperlNYC@users.noreply.github.com>
2022-11-14 14:06:12 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function redirect(page){
     location.href = page;
 }
+
+function init(){
+    var images = document.getElementsByTagName("img")
+
+    var highlightImg = function(src) {
+        alert("Works" + src);
+    };
+
+    for (var i = 0; i < images.length; i++) {
+        let src = images[i].src
+        images[i].addEventListener('click', function() {
+            highlightImg(src);
+        });
+    }
+}
+
